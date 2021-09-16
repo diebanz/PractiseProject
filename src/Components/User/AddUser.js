e4ad9105d5bd375d25b437c24f0880b88a760278
Natalia Banz <banznatalia@gmail.com>
2021-09-16 15:02:40 +0200

    Card component done

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 
 function AddUser(props) {
@@ -8,18 +9,20 @@ function AddUser(props) {
     };
 
     return (
-        <form onSubmit={addUserHandler} className={styles.form}>
-            <h1>ASSIGNMENTS</h1>
-            <label htmlFor="task">Task to do</label>
-            <input id="task" type="text"></input>
-            <label htmlFor="username">Who should do it?</label>
-            <input id="username" type="text"></input>
-            <label htmlFor="date">Deadline</label>
-            <input id="date" type="date"></input>
-            <button type="submit" className={styles["button-form"]}>
-                Assign the Task
-            </button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <h1>ASSIGNMENTS</h1>
+                <label htmlFor="task">Task to do</label>
+                <input id="task" type="text"></input>
+                <label htmlFor="username">Who should do it?</label>
+                <input id="username" type="text"></input>
+                <label htmlFor="date">Deadline</label>
+                <input id="date" type="date"></input>
+                <button type="submit" className={styles["button-form"]}>
+                    Assign the Task
+                </button>
+            </form>
+        </Card>
     );
 }
 
